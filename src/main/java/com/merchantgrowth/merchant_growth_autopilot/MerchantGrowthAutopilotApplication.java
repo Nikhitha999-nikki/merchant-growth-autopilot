357232e47c297f1b42785005bb42661dca04c1f7
Nikhitha999-nikki <nikhithamalapula@gmail.com>
2026-08-22 14:52:30 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/merchantgrowth/merchant_growth_autopilot/MerchantGrowthAutopilotApplication.java
@@ -0,0 +1,13 @@
+package com.merchantgrowth.merchant_growth_autopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantGrowthAutopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantGrowthAutopilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merchantgrowth/merchant_growth_autopilot/MerchantGrowthAutopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merchantgrowth.merchant_growth_autopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantGrowthAutopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
